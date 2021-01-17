3ba143d921088de1ce849bb0b38b8a5a4df95e7b
Roubal Sehgal <44202409+roubalsehgal@users.noreply.github.com>
2021-01-17 21:16:43 +0530

    Day17 - Count Sorted Vowel Strings

---
+++ 17.java
@@ -0,0 +1,21 @@
+// Solved this by finding a pattern (number pattern)
+// This code is written by someone else but for better understanding for future revision copied that solution
+
+class Solution {
+    public int countVowelStrings(int n) {
+        int a = 1;
+        int e = 1;
+        int i = 1;
+        int o = 1;
+        int u = 1;
+        while(n-- > 1){
+            a = a + e + i + o + u;
+            e = e + i + o + u;
+            i = i + o + u;
+            o = o + u;
+            u = u;
+        }
+        return a + e + i + o + u;
+        
+    }
+}
