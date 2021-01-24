a926c86e920dcc8ada97d2c7d7b7efb1b0e0ed1b
Roubal Sehgal <44202409+roubalsehgal@users.noreply.github.com>
2021-01-24 20:06:15 +0530

    Day24 - Merge k Sorted Lists

---
+++ 24.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        
+        for(ListNode node : lists) {
+            while(node != null) {
+                pq.add(node.val);
+                node = node.next;
+            }
+        }
+        ListNode curr = new ListNode(-1);
+        ListNode head = curr;
+        
+        while(!pq.isEmpty()) {
+            head.next = new ListNode(pq.poll());
+            head = head.next;
+        }
+        return curr.next;
+    }
+}
